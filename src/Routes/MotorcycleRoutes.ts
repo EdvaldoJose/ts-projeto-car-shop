204343ed3db7218862b2d133d5e0f69895089057
eddev <edvaldojose003@gmail.com>
2023-05-02 16:58:11 -0300

    Atualizacao eslint 11

--- src/Routes/MotorcycleRoutes.ts
+++ src/Routes/MotorcycleRoutes.ts
@@ -6,23 +6,18 @@ const motorcycleController = new MotorcycleController();
 const motorcycleRouter = Router();
 
 motorcycleRouter.post('/', (req: Request, res: Response, next: NextFunction) =>
-  motorcycleController.createMotorCycle(req, res, next),
-);
+  motorcycleController.createMotorCycle(req, res, next));
 
 motorcycleRouter.get('/:id', (req: Request, res: Response, next: NextFunction) =>
-  motorcycleController.findMotorById(req, res, next),
-);
+  motorcycleController.findMotorById(req, res, next));
 
 motorcycleRouter.get('/', (req: Request, res: Response, next: NextFunction) =>
-  motorcycleController.findAllMotoCycles(req, res, next),
-);
+  motorcycleController.findAllMotoCycles(req, res, next));
 
 motorcycleRouter.put('/:id', (req: Request, res: Response, next: NextFunction) =>
-  motorcycleController.UpdateMotorcycle(req, res, next),
-);
+  motorcycleController.UpdateMotorcycle(req, res, next));
 
 motorcycleRouter.delete('/:id', (req: Request, res: Response, next: NextFunction) =>
-  motorcycleController.DeleteVehicle(req, res, next),
-);
+  motorcycleController.DeleteVehicle(req, res, next));
 
 export default motorcycleRouter;
